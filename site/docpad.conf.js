4c8c89a4bf8018c8aa5e866c7ffa2d2e73cc7475
Sebastian Menski <sebastian.menski@googlemail.com>
2014-03-31 11:09:05 +0200

    chore(version): add 7.1 (stable) to version dropdown
    related to #CAM-2033

--- site/docpad.conf.js
+++ site/docpad.conf.js
@@ -33,7 +33,8 @@ var docpadConfig = {
         current: process.env.DOCS_VERSION || 'latest',
         all: [
           { id: 'latest', name: 'Latest' },
-          { id: '7.0', name: '7.0 (stable)' }
+          { id: '7.0', name: '7.0 (stable)' },
+          { id: '7.1', name: '7.1 (stable)' }
         ]
       },
 
