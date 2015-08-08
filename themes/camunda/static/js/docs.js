fa068bf91be8a095c94fca774d4afcf4f1cd59c8
Daniel Meyer <daniel.meyer@camunda.com>
2015-08-08 08:20:13 +0200

    chore(theme): update to latest version

--- themes/camunda/static/js/docs.js
+++ themes/camunda/static/js/docs.js
@@ -1,17 +1,297 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-/*jshint browser: true*/
+/*
+ * classList.js: Cross-browser full element.classList implementation.
+ * 1.1.20150312
+ *
+ * By Eli Grey, http://eligrey.com
+ * License: Dedicated to the public domain.
+ *   See https://github.com/eligrey/classList.js/blob/master/LICENSE.md
+ */
 
-/*global console: false*/
+/*global self, document, DOMException */
 
-function query(selector, context) {
-  context = context || document.body;
-  return context.querySelector(selector);
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js */
+
+if ("document" in self) {
+
+// Full polyfill for browsers with no classList support
+if (!("classList" in document.createElement("_"))) {
+
+(function (view) {
+
+"use strict";
+
+if (!('Element' in view)) return;
+
+var
+	  classListProp = "classList"
+	, protoProp = "prototype"
+	, elemCtrProto = view.Element[protoProp]
+	, objCtr = Object
+	, strTrim = String[protoProp].trim || function () {
+		return this.replace(/^\s+|\s+$/g, "");
+	}
+	, arrIndexOf = Array[protoProp].indexOf || function (item) {
+		var
+			  i = 0
+			, len = this.length
+		;
+		for (; i < len; i++) {
+			if (i in this && this[i] === item) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	// Vendors: please allow content code to instantiate DOMExceptions
+	, DOMEx = function (type, message) {
+		this.name = type;
+		this.code = DOMException[type];
+		this.message = message;
+	}
+	, checkTokenAndGetIndex = function (classList, token) {
+		if (token === "") {
+			throw new DOMEx(
+				  "SYNTAX_ERR"
+				, "An invalid or illegal string was specified"
+			);
+		}
+		if (/\s/.test(token)) {
+			throw new DOMEx(
+				  "INVALID_CHARACTER_ERR"
+				, "String contains an invalid character"
+			);
+		}
+		return arrIndexOf.call(classList, token);
+	}
+	, ClassList = function (elem) {
+		var
+			  trimmedClasses = strTrim.call(elem.getAttribute("class") || "")
+			, classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
+			, i = 0
+			, len = classes.length
+		;
+		for (; i < len; i++) {
+			this.push(classes[i]);
+		}
+		this._updateClassName = function () {
+			elem.setAttribute("class", this.toString());
+		};
+	}
+	, classListProto = ClassList[protoProp] = []
+	, classListGetter = function () {
+		return new ClassList(this);
+	}
+;
+// Most DOMException implementations don't allow calling DOMException's toString()
+// on non-DOMExceptions. Error's toString() is sufficient here.
+DOMEx[protoProp] = Error[protoProp];
+classListProto.item = function (i) {
+	return this[i] || null;
+};
+classListProto.contains = function (token) {
+	token += "";
+	return checkTokenAndGetIndex(this, token) !== -1;
+};
+classListProto.add = function () {
+	var
+		  tokens = arguments
+		, i = 0
+		, l = tokens.length
+		, token
+		, updated = false
+	;
+	do {
+		token = tokens[i] + "";
+		if (checkTokenAndGetIndex(this, token) === -1) {
+			this.push(token);
+			updated = true;
+		}
+	}
+	while (++i < l);
+
+	if (updated) {
+		this._updateClassName();
+	}
+};
+classListProto.remove = function () {
+	var
+		  tokens = arguments
+		, i = 0
+		, l = tokens.length
+		, token
+		, updated = false
+		, index
+	;
+	do {
+		token = tokens[i] + "";
+		index = checkTokenAndGetIndex(this, token);
+		while (index !== -1) {
+			this.splice(index, 1);
+			updated = true;
+			index = checkTokenAndGetIndex(this, token);
+		}
+	}
+	while (++i < l);
+
+	if (updated) {
+		this._updateClassName();
+	}
+};
+classListProto.toggle = function (token, force) {
+	token += "";
+
+	var
+		  result = this.contains(token)
+		, method = result ?
+			force !== true && "remove"
+		:
+			force !== false && "add"
+	;
+
+	if (method) {
+		this[method](token);
+	}
+
+	if (force === true || force === false) {
+		return force;
+	} else {
+		return !result;
+	}
+};
+classListProto.toString = function () {
+	return this.join(" ");
+};
+
+if (objCtr.defineProperty) {
+	var classListPropDesc = {
+		  get: classListGetter
+		, enumerable: true
+		, configurable: true
+	};
+	try {
+		objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+	} catch (ex) { // IE 8 doesn't support enumerable:true
+		if (ex.number === -0x7FF5EC54) {
+			classListPropDesc.enumerable = false;
+			objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+		}
+	}
+} else if (objCtr[protoProp].__defineGetter__) {
+	elemCtrProto.__defineGetter__(classListProp, classListGetter);
+}
+
+}(self));
+
+} else {
+// There is full or partial native classList support, so just check if we need
+// to normalize the add/remove and toggle APIs.
+
+(function () {
+	"use strict";
+
+	var testElement = document.createElement("_");
+
+	testElement.classList.add("c1", "c2");
+
+	// Polyfill for IE 10/11 and Firefox <26, where classList.add and
+	// classList.remove exist but support only one argument at a time.
+	if (!testElement.classList.contains("c2")) {
+		var createMethod = function(method) {
+			var original = DOMTokenList.prototype[method];
+
+			DOMTokenList.prototype[method] = function(token) {
+				var i, len = arguments.length;
+
+				for (i = 0; i < len; i++) {
+					token = arguments[i];
+					original.call(this, token);
+				}
+			};
+		};
+		createMethod('add');
+		createMethod('remove');
+	}
+
+	testElement.classList.toggle("c3", false);
+
+	// Polyfill for IE 10 and Firefox <24, where classList.toggle does not
+	// support the second argument.
+	if (testElement.classList.contains("c3")) {
+		var _toggle = DOMTokenList.prototype.toggle;
+
+		DOMTokenList.prototype.toggle = function(token, force) {
+			if (1 in arguments && !this.contains(token) === !force) {
+				return force;
+			} else {
+				return _toggle.call(this, token);
+			}
+		};
+
+	}
+
+	testElement = null;
+}());
+
+}
+
 }
 
 
+},{}],2:[function(require,module,exports){
+'use strict';
+/*jshint browser: true*/
+
+/*global require: false, console: false*/
+
+require('./classList');
+
+function toArray(thing) {
+  var arr = [];
+  if (!thing.length) { return arr; }
+  for (var i = 0; i < thing.length; i++) {
+    arr.push(thing[i]);
+  }
+  return arr;
+}
+
+function attr(node, name, value) {
+  if (value === null) {
+    node.removeAttribute(name);
+  }
+  else if (typeof value !== 'undefined') {
+    node.setAttribute(name, value);
+  }
+  return node.getAttribute(name);
+}
+
+function offset(node) {
+  var parent = node;
+  var offset = {
+    top: parent.offsetTop,
+    left: parent.offsetLeft,
+  };
+
+  while ((parent = parent.offsetParent)) {
+    offset.top += parent.offsetTop;
+    offset.left += parent.offsetLeft;
+  }
+
+  return offset;
+}
+
+function query(selector, context) {
+  context = (context || document.body);
+  return context.querySelector(selector);
+}
+
+function queryAll(selector, context) {
+  context = (context || document.body);
+  return toArray(context.querySelectorAll(selector));
+}
+
 function openParentItem(childItem) {
-  childItem.className = 'open';
+  childItem.classList.add('open');
 
   var parentItem = childItem.parentNode.parentNode;
   if (parentItem.tagName.toLowerCase() === 'li') {
@@ -20,14 +300,75 @@ function openParentItem(childItem) {
 }
 
 
-var currentLink = query('[href$="' + location.pathname + '"]');
+
+
+
+
+var toc = query('#TableOfContents');
+
+var tocLeft = 0;
+var tocWrapper;
+var tocLinks;
+var tocTargets;
+var tocTargetPositions;
+
+var currentLink = query('.site-menu a[href$="' + location.pathname + '"]');
 var currentMenuItem;
+
+function tocPosition() {
+  if (toc) {
+    attr(tocWrapper, 'style', null);//.style.position = 'static';
+    tocLeft = offset(tocWrapper).left;
+    tocWrapper.style.width = tocWrapper.clientWidth + 'px';
+    tocWrapper.style.left = tocLeft + 'px';
+    tocWrapper.style.position = 'fixed';
+  }
+}
+
+if (toc) {
+  tocWrapper = toc.parentNode;
+  tocPosition();
+
+  tocLinks = queryAll('a[href]', toc);
+
+  tocTargets = tocLinks.map(function (node) {
+    return document.getElementById(attr(node, 'href').slice(1));
+  });
+
+  tocTargetPositions = tocTargets.map(function (node) {
+    return offset(node);
+  });
+}
 
 if (currentLink) {
   currentMenuItem = currentLink.parentNode;
   openParentItem(currentLink.parentNode);
 }
 
-console.info(currentLink, currentMenuItem, location.path);
 
-},{}]},{},[1]);
+function scrolling() {
+  if (!toc) { return; }
+
+  var top = window.scrollY;
+  // toc.parentNode.style.top = (top + tocOffset) + 'px';
+
+  tocLinks.forEach(function (node) {
+    node.parentNode.classList.remove('open');
+  });
+
+  for (var i = 0; i < tocTargetPositions.length; i++) {
+    if (tocTargetPositions[i].top >= top) {
+      tocLinks[i].parentNode.classList.add('open');
+      openParentItem(tocLinks[i].parentNode);
+      i = tocTargetPositions.length;
+    }
+  }
+}
+
+window.addEventListener('resize', function () {
+  tocPosition();
+});
+window.addEventListener('scroll', scrolling);
+scrolling();
+
+},{"./classList":1}]},{},[2]);
