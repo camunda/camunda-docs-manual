0875590d8630f5be005ab5e3a49347791584d917
Daniel Meyer <daniel.meyer@camunda.com>
2015-11-27 10:33:39 +0100

    chore(release): add 7.4

--- site/docpad.conf.js
+++ site/docpad.conf.js
@@ -40,6 +40,7 @@ module.exports = {
         current: process.env.DOCS_VERSION || '7.2',
         all: [
           { id: 'latest', name: 'Latest' },
+          { id: '7.4', name: '7.4 (stable)' },
           { id: '7.3', name: '7.3 (stable)' },
           { id: '7.2', name: '7.2 (stable)' },
           { id: '7.1', name: '7.1 (stable)' },
