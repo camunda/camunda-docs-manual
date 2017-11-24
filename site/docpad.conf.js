a7033bfaea11d53ef8a049121a68dd33876a907e
Yana Vasileva <yana.vasileva@camunda.com>
2017-11-24 15:06:40 +0100

    chore(release): add 7.8

--- site/docpad.conf.js
+++ site/docpad.conf.js
@@ -40,6 +40,7 @@ module.exports = {
         current: process.env.DOCS_VERSION || '7.1',
         all: [
           { id: 'latest', name: 'Latest' },
+          { id: '7.8', name: '7.8 (stable)' },
           { id: '7.7', name: '7.7 (stable)' },
           { id: '7.6', name: '7.6 (stable)' },
           { id: '7.5', name: '7.5 (stable)' },
